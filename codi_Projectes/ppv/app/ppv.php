00cb7984c75a9558071a6bfa578e70745b05b7b3
vimasaAsix <vsaulm15@gmail.com>
2023-10-15 15:38:52 +0200

    Commit 2 - grup04 -Activitat de gp1f4a5

--- codi_Projectes/ppv/app/ppv.php
+++ codi_Projectes/ppv/app/ppv.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="utf-8">
-        <title>PRICE OF THE SELECTED MATCH</title>
+    <meta charset="utf-8">
+    <title>PRICE OF THE SELECTED MATCH</title>
 </head>
-        <body>
-                <p><u>PRICE OF THE SELECTED MATCH</u></p>
-                <?php
-			if ($_GET["e1"]==$_GET["e2"]){
-				echo "Wrong selection. A team can not play against itself. Select a different team 2, please</br>";
-			}
-			else{
-				if ((($_GET["e1"]=="a") and  ($_GET["e2"]=="b")) or (($_GET["e1"]=="b") and  ($_GET["e2"]=="a"))){
-					echo "Special match: Price: 15 Euros</br>";
-				}
-				else {
-					echo "Basic match. Price: 10 Euros</br>";
-				}
-			}
-		?>
-		<br>
-		<a href="index.html">Return to the init web page</a>
-        </body>
+<body>
+    <p><u>PRICE OF THE SELECTED MATCH</u></p>
+    <?php
+    if ($_GET["e1"] == $_GET["e2"]) {
+        echo "Wrong selection. A team cannot play against itself. Select a different team 2, please</br>";
+    } else {
+        if (
+            ($_GET["e1"] == "b" && $_GET["e2"] == "d") ||
+            ($_GET["e1"] == "d" && $_GET["e2"] == "b")
+        ) {
+            echo "Special match: Price: 13 Euros</br>";
+        } else {
+            echo "Basic match. Price: 10 Euros</br>";
+        }
+    }
+    ?>
+    <br>
+    <a href="index.html">Return to the init web page</a>
+</body>
 </html>
